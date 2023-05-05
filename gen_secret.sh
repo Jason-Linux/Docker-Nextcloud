442f8bf40409d90b50854b97e0fc9bb2e08c2973
jason-linux <dubois.jason8926@gmail.com>
2023-05-05 03:15:24 +0200

    gen_secret.sh repair directories

--- gen_secret.sh
+++ gen_secret.sh
@@ -14,19 +14,19 @@ read ANSWER
 if [ -z $ANSWER ] || [ $ANSWER = 'Y' ]
 then
 	for VARIABLE in $TO_GENERATE ; do
-        	echo $(generatePassword) > $(dirname "$0")/secrets/mysql-${VARIABLE}.txt
+        	echo $(generatePassword) > $(dirname "$0")/.secrets/mysql-${VARIABLE}.txt
 	done
 	echo "Database secrets initialized whit random data"
 else 
 	echo "What is your database name ?"
 	read DATABASE
-	echo $DATABASE > $(dirname "$0")/secrets/mysql-database.txt
+	echo $DATABASE > $(dirname "$0")/.secrets/mysql-database.txt
 	echo "What is your database user ?"
 	read USER
-	echo $USER > $(dirname "$0")/secrets/mysql-user.txt
+	echo $USER > $(dirname "$0")/.secrets/mysql-user.txt
 	echo "User password ?"
 	read PASSWORD
-	echo $PASSWORD > $(dirname "$0")/secrets/mysql-password.txt
+	echo $PASSWORD > $(dirname "$0")/.secrets/mysql-password.txt
 	echo "Database secrets initialized whit your access"
 fi
 
